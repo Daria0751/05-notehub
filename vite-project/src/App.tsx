3888b8e51b9326d100c58b776a0ee31a1ec45d5f
Daria <darak0751@gmail.com>
2025-06-05 13:48:17 +0300

    hw3

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import { Movie } from './types/movie';
+import { fetchMovies } from './services/movieService';
+import SearchBar from './components/SearchBar/SearchBar';
+import MovieGrid from './components/MovieGrid/MovieGrid';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import MovieModal from './components/MovieModal/MovieModal';
+import toast from 'react-hot-toast';
+
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const results = await fetchMovies(query);
+
+        if (results.length === 0) {
+          toast('No movies found for your request.');
+        }
+
+        setMovies(results);
+      } catch (err) {
+        setError(true);
+        toast.error('Something went wrong!');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [query]);
+
+  const handleSearch = (newQuery: string) => {
+    if (!newQuery.trim()) {
+      toast('Please enter your search query.');
+      return;
+    }
+    setQuery(newQuery);
+    setMovies([]);
+  };
+
+  const handleSelect = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+  };
+
+  return (
+    <div>
+      <SearchBar onSubmit={handleSearch} />
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {!loading && !error && movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={handleSelect} />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
+    </div>
+  );
+};
+
+export default App;
