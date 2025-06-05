d8e7ec85116dc672728d2707e4db3493a70065a1
Daria <darak0751@gmail.com>
2025-06-05 20:59:21 +0300

    hw3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import type { Movie } from './types/movie';
 import { fetchMovies } from './services/movieService';
-import SearchBar from './components/SearchBar/SearchBar';
+import SearchBar from './components/SearchBar';
 import MovieGrid from './components/MovieGrid/MovieGrid';
 import Loader from './components/Loader/Loader';
 import ErrorMessage from './components/ErrorMessage/ErrorMessage';
