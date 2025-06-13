baba6821c194ff9c422fff89b4a5b1cae62f0079
Daria <darak0751@gmail.com>
2025-06-13 22:25:29 +0300

    hw5

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,5 +1,17 @@
+import { useField } from 'formik';
 import styles from './ErrorMessage.module.css';
 
-export default function ErrorMessage() {
-  return <p className={styles.text}>There was an error, please try again...</p>;
+interface ErrorMessageProps {
+  name: string;
+  className?: string;
+}
+
+export default function ErrorMessage({ name, className }: ErrorMessageProps) {
+  const [, meta] = useField(name);
+
+  if (!meta.touched || !meta.error) {
+    return null;
+  }
+
+  return <p className={className || styles.text}>{meta.error}</p>;
 }
